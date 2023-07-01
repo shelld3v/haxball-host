a8e3a28c2201665cb3c62833f88548b6a068580f
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2023-07-01 13:45:06 +0700

    Change password

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ADMIN_PASSWORD = "depao";
+const ADMIN_PASSWORD = "svdp";
 const START_GAME_COMMENT = `Chào mừng đến với Cà Khịa TV, tôi là Trông Anh Ngược, BLV của các bạn ngày hôm nay`;
 const YELLOW = 0xFFEA00;
 const RED = 0xFF0000;
