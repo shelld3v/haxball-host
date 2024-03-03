a98d88f6ea85ac2a5b12e76e3c8200c580985975
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2024-03-03 21:40:15 +0700

    Bug fix

--- script.js
+++ script.js
@@ -538,8 +538,8 @@ async function celebrationEffect(player, hasScored) {
         room.setPlayerDiscProperties(
           players[i],
           {
-            x: player.x + stadium.playerRadius * 3 * math.cos(math.PI * i / players.length),
-            y: player.y + stadium.playerRadius * 3 * math.sin(math.PI * i / players.length)
+            x: player.x + stadium.playerRadius * 3 * Math.cos(Math.PI * i / players.length),
+            y: player.y + stadium.playerRadius * 3 * Math.sin(Math.PI * i / players.length)
           }
         );
       };
