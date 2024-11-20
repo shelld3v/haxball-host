e71ada4bfb31fa4783df96b41803e26037f6178e
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2024-11-20 17:08:42 +0700

    Change kick rate limit

--- script.js
+++ script.js
@@ -377,7 +377,7 @@ var room = HBInit({
   public: true,
 });
 room.setTeamsLock(1);
-room.setKickRateLimit(6, 0, 0);
+room.setKickRateLimit(6, 12, 5);
 loadStadium("training").then(_ => { room.startGame() });
 setInterval(randomAnnouncement, NOTIFICATION_INTERVAL * 1000);
 setInterval(randomGameStat, 2.5 * 60 * 1000);
